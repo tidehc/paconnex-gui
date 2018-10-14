bd271afdee678e3cd864a05148cb7c980ebba7f0
pacomms <38086033+pacomms@users.noreply.github.com>
2018-10-14 22:51:46 +0100

    update upgrade text
    na

--- core/upgrade/index.php
+++ core/upgrade/index.php
@@ -154,7 +154,7 @@
 		if (($branch_return_value == 0) && ($commit_return_value == 0)) {
 			echo $text['label-git_branch'].' '.$git_current_branch." \n";
 			//echo $text['label-git_commit'].' '." ";
-			echo "<a href='https://github.com/fusionpbx/fusionpbx/compare/";
+			echo "<a href='https://github.com/pacomms/paconnex-gui/compare/";
 			echo $git_current_commit . "..." . "$git_current_branch' target='_blank'> \n";
 			echo $git_current_commit . "</a><br />\n";
 			echo "</a>";
